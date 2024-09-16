e327978df0c5b283bd3d1f8c087559bd33d55987
segunemma2003 <segunemma2003@gmail.com>
2024-09-16 23:10:36 +0100

    new commit

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -27,12 +27,12 @@ class UserResource extends Resource
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->unique(table: User::class)
-                    ->dehydrated(false)
-                    ->maxLength(255)
-                    ->disabledOn('edit'),
+                // Forms\Components\TextInput::make('email')
+                //     ->email()
+                //     ->unique(table: User::class)
+                //     ->dehydrated(false)
+                //     ->maxLength(255)
+                //     ->disabledOn('edit'),
 
                 // Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')

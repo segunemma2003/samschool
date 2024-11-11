3bc86682d619e17cfaabfa1cacdb1b148469d649
segunemma2003 <segunemma2003@gmail.com>
2024-11-11 16:02:59 +0100

    a

--- app/Filament/Teacher/Resources/ExamResource/Pages/CreateExam.php
+++ app/Filament/Teacher/Resources/ExamResource/Pages/CreateExam.php
@@ -3,10 +3,18 @@
 namespace App\Filament\Teacher\Resources\ExamResource\Pages;
 
 use App\Filament\Teacher\Resources\ExamResource;
+use App\Models\AcademicYear;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateExam extends CreateRecord
 {
     protected static string $resource = ExamResource::class;
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $academy = AcademicYear::whereStatus('true')->first();
+        $data['academic_year_id'] = $academy->id ?? 1;
+
+        return $data;
+    }
 }

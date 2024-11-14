9ebf4f185d99415467b1f00bb6be90df536da6cc
segunemma2003 <segunemma2003@gmail.com>
2024-11-14 03:45:26 +0100

    a

--- app/Filament/Teacher/Resources/StudentResource/Pages/CreateStudent.php
+++ app/Filament/Teacher/Resources/StudentResource/Pages/CreateStudent.php
@@ -3,10 +3,38 @@
 namespace App\Filament\Teacher\Resources\StudentResource\Pages;
 
 use App\Filament\Teacher\Resources\StudentResource;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class CreateStudent extends CreateRecord
 {
     protected static string $resource = StudentResource::class;
+
+    public function afterCreate()
+    {
+        $data = $this->getRecord();
+        Log::info($data);
+        $mindex = User::max('id')+1;
+        Log::info($mindex);
+        $email = str_replace(' ', '.', $data['name']). $mindex.'@'.request()->getHost();
+        $username = str_replace(' ', '.', $data['name']). $mindex;
+        $user = User::updateOrCreate([
+            "name"=> $data['name'],
+            "email"=>  $email,
+            "password"=>Hash::make($data["password"]),
+            "username"=> $username,
+            "user_type"=>"student"
+        ]);
+
+
+
+        $this->getRecord()->update([
+            "email"=> $email,
+            "username"=> $username
+        ]);
+
+    }
 }

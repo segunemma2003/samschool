49fd7e430a5ea428f52d72ba6fc5ea929b13596a
segunemma2003 <segunemma2003@gmail.com>
2024-11-19 11:43:54 +0100

    a

---
+++ app/Filament/Teacher/Resources/TeacherResource/Pages/CreateTeacher.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Teacher\Resources\TeacherResource\Pages;
+
+use App\Filament\Teacher\Resources\TeacherResource;
+use App\Models\User;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class CreateTeacher extends CreateRecord
+{
+    protected static string $resource = TeacherResource::class;
+
+    public function afterCreate()
+    {
+        $data = $this->getRecord();
+        Log::info($data);
+
+        $user = User::updateOrCreate([
+            "name"=> $data['name'],
+            "email"=> $data['email'],
+            "password"=>Hash::make($data["password"]),
+            "username"=>$data["username"],
+             "user_type"=>"teacher"
+        ]);
+    }
+}
---
+++ app/Filament/Teacher/Resources/TeacherResource/Pages/ListTeachers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Teacher\Resources\TeacherResource\Pages;
+
+use App\Filament\Teacher\Resources\TeacherResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListTeachers extends ListRecords
+{
+    protected static string $resource = TeacherResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
